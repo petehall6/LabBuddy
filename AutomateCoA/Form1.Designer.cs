e4451952dde2b53db45298531a26e46234969ab3
petehall6 <peter.hall6@gmail.com>
2022-08-19 14:14:42 -0500

    Initial commit

---
+++ AutomateCoA/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace AutomateCoA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.FetchBtn1 = new System.Windows.Forms.Button();
+            this.ItemBox1 = new System.Windows.Forms.TextBox();
+            this.LotBox1 = new System.Windows.Forms.TextBox();
+            this.VendorBox1 = new System.Windows.Forms.ComboBox();
+            this.clrCoABtn = new System.Windows.Forms.Button();
+            this.QRRFindBtn = new System.Windows.Forms.Button();
+            this.QRRLabel = new System.Windows.Forms.Label();
+            this.CoALabel = new System.Windows.Forms.Label();
+            this.ComboPDFSection = new System.Windows.Forms.Label();
+            this.QRRFileNameBx = new System.Windows.Forms.TextBox();
+            this.QRRPDFBtn = new System.Windows.Forms.Button();
+            this.CoAPDFBtn = new System.Windows.Forms.Button();
+            this.CoAFileNameBx = new System.Windows.Forms.TextBox();
+            this.SaveFinalPDFBtn = new System.Windows.Forms.Button();
+            this.PDFNameLabel = new System.Windows.Forms.Label();
+            this.FinalPDFBx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // FetchBtn1
+            // 
+            resources.ApplyResources(this.FetchBtn1, "FetchBtn1");
+            this.FetchBtn1.Name = "FetchBtn1";
+            this.FetchBtn1.UseVisualStyleBackColor = true;
+            this.FetchBtn1.Click += new System.EventHandler(this.FetchBtn1_Click);
+            // 
+            // ItemBox1
+            // 
+            this.ItemBox1.AcceptsTab = true;
+            resources.ApplyResources(this.ItemBox1, "ItemBox1");
+            this.ItemBox1.Name = "ItemBox1";
+            // 
+            // LotBox1
+            // 
+            this.LotBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.LotBox1, "LotBox1");
+            this.LotBox1.Name = "LotBox1";
+            // 
+            // VendorBox1
+            // 
+            resources.ApplyResources(this.VendorBox1, "VendorBox1");
+            this.VendorBox1.FormattingEnabled = true;
+            this.VendorBox1.Items.AddRange(new object[] {
+            resources.GetString("VendorBox1.Items"),
+            resources.GetString("VendorBox1.Items1"),
+            resources.GetString("VendorBox1.Items2"),
+            resources.GetString("VendorBox1.Items3"),
+            resources.GetString("VendorBox1.Items4")});
+            this.VendorBox1.Name = "VendorBox1";
+            // 
+            // clrCoABtn
+            // 
+            resources.ApplyResources(this.clrCoABtn, "clrCoABtn");
+            this.clrCoABtn.Name = "clrCoABtn";
+            this.clrCoABtn.UseVisualStyleBackColor = true;
+            this.clrCoABtn.Click += new System.EventHandler(this.clrCoABtn_Click);
+            // 
+            // QRRFindBtn
+            // 
+            resources.ApplyResources(this.QRRFindBtn, "QRRFindBtn");
+            this.QRRFindBtn.Name = "QRRFindBtn";
+            this.QRRFindBtn.UseVisualStyleBackColor = true;
+            this.QRRFindBtn.Click += new System.EventHandler(this.QRRFindBtn_Click);
+            // 
+            // QRRLabel
+            // 
+            resources.ApplyResources(this.QRRLabel, "QRRLabel");
+            this.QRRLabel.Name = "QRRLabel";
+            // 
+            // CoALabel
+            // 
+            resources.ApplyResources(this.CoALabel, "CoALabel");
+            this.CoALabel.Name = "CoALabel";
+            // 
+            // ComboPDFSection
+            // 
+            resources.ApplyResources(this.ComboPDFSection, "ComboPDFSection");
+            this.ComboPDFSection.Name = "ComboPDFSection";
+            // 
+            // QRRFileNameBx
+            // 
+            resources.ApplyResources(this.QRRFileNameBx, "QRRFileNameBx");
+            this.QRRFileNameBx.Name = "QRRFileNameBx";
+            // 
+            // QRRPDFBtn
+            // 
+            resources.ApplyResources(this.QRRPDFBtn, "QRRPDFBtn");
+            this.QRRPDFBtn.Name = "QRRPDFBtn";
+            this.QRRPDFBtn.UseVisualStyleBackColor = true;
+            this.QRRPDFBtn.Click += new System.EventHandler(this.QRRPDFBtn_Click);
+            // 
+            // CoAPDFBtn
+            // 
+            resources.ApplyResources(this.CoAPDFBtn, "CoAPDFBtn");
+            this.CoAPDFBtn.Name = "CoAPDFBtn";
+            this.CoAPDFBtn.UseVisualStyleBackColor = true;
+            this.CoAPDFBtn.Click += new System.EventHandler(this.CoAPDFBtn_Click);
+            // 
+            // CoAFileNameBx
+            // 
+            resources.ApplyResources(this.CoAFileNameBx, "CoAFileNameBx");
+            this.CoAFileNameBx.Name = "CoAFileNameBx";
+            // 
+            // SaveFinalPDFBtn
+            // 
+            resources.ApplyResources(this.SaveFinalPDFBtn, "SaveFinalPDFBtn");
+            this.SaveFinalPDFBtn.Name = "SaveFinalPDFBtn";
+            this.SaveFinalPDFBtn.UseVisualStyleBackColor = true;
+            this.SaveFinalPDFBtn.Click += new System.EventHandler(this.SaveFinalPDFBtn_Click);
+            // 
+            // PDFNameLabel
+            // 
+            resources.ApplyResources(this.PDFNameLabel, "PDFNameLabel");
+            this.PDFNameLabel.Name = "PDFNameLabel";
+            // 
+            // FinalPDFBx
+            // 
+            resources.ApplyResources(this.FinalPDFBx, "FinalPDFBx");
+            this.FinalPDFBx.Name = "FinalPDFBx";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SaveFinalPDFBtn);
+            this.Controls.Add(this.PDFNameLabel);
+            this.Controls.Add(this.FinalPDFBx);
+            this.Controls.Add(this.CoAFileNameBx);
+            this.Controls.Add(this.CoAPDFBtn);
+            this.Controls.Add(this.QRRPDFBtn);
+            this.Controls.Add(this.QRRFileNameBx);
+            this.Controls.Add(this.ComboPDFSection);
+            this.Controls.Add(this.CoALabel);
+            this.Controls.Add(this.QRRLabel);
+            this.Controls.Add(this.QRRFindBtn);
+            this.Controls.Add(this.clrCoABtn);
+            this.Controls.Add(this.VendorBox1);
+            this.Controls.Add(this.LotBox1);
+            this.Controls.Add(this.ItemBox1);
+            this.Controls.Add(this.FetchBtn1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FetchBtn1;
+        private System.Windows.Forms.TextBox ItemBox1;
+        private System.Windows.Forms.TextBox LotBox1;
+        private System.Windows.Forms.ComboBox VendorBox1;
+        private System.Windows.Forms.Button clrCoABtn;
+        private System.Windows.Forms.Button QRRFindBtn;
+        private System.Windows.Forms.Label QRRLabel;
+        private System.Windows.Forms.Label CoALabel;
+        private System.Windows.Forms.Label ComboPDFSection;
+        private System.Windows.Forms.TextBox QRRFileNameBx;
+        private System.Windows.Forms.Button QRRPDFBtn;
+        private System.Windows.Forms.Button CoAPDFBtn;
+        private System.Windows.Forms.TextBox CoAFileNameBx;
+        private System.Windows.Forms.Button SaveFinalPDFBtn;
+        private System.Windows.Forms.Label PDFNameLabel;
+        private System.Windows.Forms.TextBox FinalPDFBx;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AutomateCoA/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Remote;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using Microsoft.Office.Interop.Word;
+using Spire.Pdf;
+
+
+
+namespace AutomateCoA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //open and edit QRR
+        //TODO make new directory with QRR and labels .docs.
+        //TODO set as default directory
+        private void QRRFindBtn_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog _fileDialog = new OpenFileDialog();
+            _fileDialog.InitialDirectory = "Z:\\SJShare\\SJCOMMON\\DI\\MIC\\COMPONENT DOCS & VALIDATIONS\\Quarantine Release\\Digital QRR - Test Set";
+
+            if (_fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //launch and open an instance of word
+                Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+                object fileName = _fileDialog.FileName;
+                object readOnly = false;
+                object isVisible = true;
+                //easy way to handle random refs I dont care about
+                object iDontCare = System.Reflection.Missing.Value;
+
+                wordApp.Visible = true;
+
+                Microsoft.Office.Interop.Word.Document qrrDoc = wordApp.Documents.Open(ref fileName, ref iDontCare, ref readOnly,
+                                                                                        ref iDontCare, ref iDontCare, ref iDontCare,
+                                                                                        ref iDontCare, ref iDontCare, ref iDontCare,
+                                                                                        ref iDontCare, ref iDontCare, ref isVisible,
+                                                                                        ref iDontCare, ref iDontCare, ref iDontCare, ref iDontCare);
+                qrrDoc.Activate();
+            }
+        }
+
+
+        //find and save CoA
+        //TODO add other vendors
+        private void FetchBtn1_Click(object sender, EventArgs e)
+        {
+            //iniate web browser with console hidden.  Browser will close when driver is exited.
+            ChromeOptions options = new ChromeOptions();
+            options.LeaveBrowserRunning = false;
+            
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            IWebDriver mydriver = new ChromeDriver(service, options);
+
+            //when waiting on elements to load
+            WebDriverWait wait = new WebDriverWait(mydriver, TimeSpan.FromSeconds(10));
+
+            //get input from text/comboboxes
+            switch (VendorBox1.Text.ToUpper())
+            {
+                case "FISHER":
+                    
+                    mydriver.Url = "https://www.fishersci.com/us/en/catalog/search/certificates.html";
+                    //catalog number
+                    mydriver.FindElement(By.XPath("/html/body/section/section/div/div/div/div[2]/div[1]/div/form/ul/li[1]/input")).SendKeys(ItemBox1.Text);
+                    //lot number
+                    mydriver.FindElement(By.XPath("/html/body/section/section/div/div/div/div[2]/div[1]/div/form/ul/li[2]/input")).SendKeys(LotBox1.Text);
+                    //document type.  HAVE TO HIT RETURN TO CONFIRM SELECTION OR IT ALL GOES TO HELL!!!!
+                    mydriver.FindElement(By.XPath("/html/body/section/section/div/div/div/div[2]/div[1]/div/form/ul/li[3]/select")).SendKeys("C");
+                    mydriver.FindElement(By.XPath("/html/body/section/section/div/div/div/div[2]/div[1]/div/form/ul/li[3]/select")).SendKeys(OpenQA.Selenium.Keys.Return);
+                    //search button
+                    mydriver.FindElement(By.CssSelector("#certificate-search-form > form > ul > li:nth-child(4) > input")).Click();
+                    //certificate inside of table     
+                    mydriver.FindElement(By.XPath("/html/body/section/section/div/div/div/div[3]/table/tbody/tr/td[6]/a")).Click();
+                             
+
+                    //close window but not exit webdriver
+                    mydriver.Close();
+                    break;
+
+                case "SIGMA":
+                    mydriver.Url = "https://www.sigmaaldrich.com/US/en/search";
+
+                    //click on cookie accept button
+                    IWebElement cookiebtn = wait.Until(drvr => drvr.FindElement(By.CssSelector("#onetrust-close-btn-container > button")));
+                    cookiebtn.Click();
+             
+                    //product number
+                    mydriver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div/div/div[2]/div/div[1]/form/div/div[1]/div/div/div/input")).SendKeys(ItemBox1.Text);
+                    //lot number
+                    mydriver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div/div/div[2]/div/div[1]/form/div/div[3]/div/div/div/input")).SendKeys(LotBox1.Text);
+                    //search button
+                    mydriver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div/div/div[2]/div/div[1]/form/div/button/span")).Click();
+
+                    //TODO why cant I close the tab??!?! without closing all the thigns
+                    //mydriver.Close();
+
+                    break;
+
+                case "VWR":
+                    mydriver.Url = "https://us.vwr.com/store/search/searchCerts.jsp?tabId=certSearch";
+
+                    //part number
+
+                    mydriver.FindElement(By.XPath("//*[@id='certSearchPartNumber']")).SendKeys(ItemBox1.Text);
+                    //lot number
+                    mydriver.FindElement(By.XPath("//*[@id='certSearchLotNumber']")).SendKeys(LotBox1.Text);
+                    //search button
+                    mydriver.FindElement(By.XPath("//*[@id='certSearch']")).Click();
+                    //certificate inside of table
+                    mydriver.FindElement(By.XPath("//*[@id='content']/div/div[2]/div[2]/div/div/table/tbody[1]/tr/td[2]/a")).Click();              
+
+                    //mydriver.Close();
+
+                    break;
+
+                case "BD":
+                    mydriver.Url = "https://regdocs.bd.com/regdocs/qcinfo";
+
+
+                    mydriver.FindElement(By.XPath("//*[@id='page']/div/section[2]/div/div/div/div/div/form/div[2]/div[1]/div/div[1]/input")).SendKeys(ItemBox1.Text);
+                    //lot number
+                    mydriver.FindElement(By.XPath("//*[@id='page']/div/section[2]/div/div/div/div/div/form/div[2]/div[1]/div/div[2]/input")).SendKeys(LotBox1.Text);
+                    //search button
+                    mydriver.FindElement(By.XPath("//*[@id='page']/div/section[2]/div/div/div/div/div/form/button[2]")).Click();
+                    //certificate inside of table
+                    //mydriver.FindElement(By.XPath("//*[@id='page']/div/section[1]/div/div[2]/div[1]/div/div[2]/table/tbody/tr[2]/td[5]/i")).Click();
+
+                    //click on cookie accept button
+                    IWebElement certbtn = wait.Until(drvr => drvr.FindElement(By.XPath("//*[@id='page']/div/section[1]/div/div[2]/div[1]/div/div[2]/table/tbody/tr[2]/td[5]/i")));
+                    certbtn.Click();
+
+                    break;
+
+                case "MILLIPORE":
+                    mydriver.Url = "https://www.emdmillipore.com/US/en/documents/Z.qb.qB.tecAAAFDDJUsznLq,nav#s_mbqBTWQAAAFKyVsIik_d";
+
+                    //catalog number  ItemBox1.Text
+                    mydriver.FindElement(By.XPath("//*[@id='COAOrderNumber']")).SendKeys("1460040020");
+                    //lot number  LotBox1.Text
+                    mydriver.FindElement(By.XPath("//*[@id='COABatchNumber']")).SendKeys("726353");
+                    mydriver.FindElement(By.XPath("//*[@id='COABatchNumber']")).SendKeys(OpenQA.Selenium.Keys.Return);
+                    //search button
+                    //mydriver.FindElement(By.XPath("//*[@id='s_mbqBTWQAAAFKyVsIik_d_find']")).SendKeys(OpenQA.Selenium.Keys.Return);
+                    //certificate inside of table
+
+                    //*[@id="s_mbqBTWQAAAFKyVsIik_d_find"]
+
+                    //certbtn = wait.Until(drvr => drvr.FindElement(By.XPath("//*[@id='result - FvCbqBDaYAAAFErYZWba4i']/table/tbody/tr/td[1]/a")));
+                    //certbtn.Click();
+
+                    break;
+            }
+        }
+
+        //clear CoA Buttons
+        private void clrCoABtn_Click(object sender, EventArgs e)
+        {
+            ItemBox1.Text = "Item#";
+            LotBox1.Text = "Lot#";
+            VendorBox1.Text = "Vendor";
+            QRRFileNameBx.Text = "";
+            CoAFileNameBx.Text = "";
+            FinalPDFBx.Text = "";
+
+        }
+
+
+        //shared scope
+        private string qrrPDFPath;
+        private string coaPDFPath;
+
+
+        //merge and save QRR and CoA PDF
+        private void QRRPDFBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog _fileDialog = new OpenFileDialog();
+            _fileDialog.ShowDialog();
+
+            qrrPDFPath = _fileDialog.FileName.ToString();
+
+            QRRFileNameBx.Text = qrrPDFPath;
+
+            
+        }
+
+        private void CoAPDFBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog _fileDialog = new OpenFileDialog();
+            _fileDialog.ShowDialog();
+
+            coaPDFPath = _fileDialog.FileName.ToString();
+
+            CoAFileNameBx.Text = coaPDFPath;
+        }
+
+        //merge, rename and save final PDF
+        private void SaveFinalPDFBtn_Click(object sender, EventArgs e)
+        {
+            //store paths in string array
+            string[] pdfPaths = { qrrPDFPath, coaPDFPath };
+
+            //load all pdfs into an PdfDocument obj and store all objs in PdfDocument array
+            PdfDocument[] pdfs = new PdfDocument[pdfPaths.Length];
+
+            for (int i=0; i< pdfPaths.Length; i++)
+            {
+                pdfs[i] = new PdfDocument(pdfPaths[i]);
+            }
+
+            PdfDocument finalPdf = new PdfDocument();
+
+            //just first page of QRR and all pages of CoA
+            finalPdf.InsertPage(pdfs[0], 0);
+            finalPdf.AppendPage(pdfs[1]);
+
+            string finalPdfPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            
+
+            string finalPdfName = FinalPDFBx.Text;
+
+            finalPdf.SaveToFile(finalPdfPath+"\\"+ finalPdfName + ".pdf");
+            Process.Start(finalPdfPath + "\\" + finalPdfName +".pdf");
+
+
+
+
+
+        }
+    }
+}

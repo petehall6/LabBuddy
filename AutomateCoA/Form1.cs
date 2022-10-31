37951308aa97a509db729dac1a7dfb04e7f564a3
petehall6 <peter.hall6@gmail.com>
2022-10-31 15:04:51 -0500

    10/31/2022
    Working on exception handling.  Need to finalize storage folder into Added to FIIX folder after demo.

--- AutomateCoA/Form1.cs
+++ AutomateCoA/Form1.cs
@@ -22,18 +22,6 @@ using System.Runtime.CompilerServices;
 using Microsoft.Office.Core;
 //using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
-
-/*TODO
- *
- * design
- * make new directory with QRR and labels .docs.
- * fix exception handling
- * 
- */
-
-
-
-
 namespace AutomateCoA
 {
     public partial class Form1 : Form
@@ -110,10 +98,11 @@ namespace AutomateCoA
         //open and edit QRR
         private void FindQRR()
         {
+            
             if(bcrMode == false)
             {
                 OpenFileDialog _fileDialog = new OpenFileDialog();
-                _fileDialog.InitialDirectory = "Z:\\SJShare\\SJCOMMON\\DI\\MIC\\COMPONENT DOCS & VALIDATIONS\\Quarantine Release\\Digital QRR - Test Set";
+                _fileDialog.InitialDirectory = "Z:\\SJShare\\SJCOMMON\\DI\\MIC\\COMPONENT DOCS & VALIDATIONS\\Quarantine Release\\Digital QRR - Lab Buddy";
 
                 if (_fileDialog.ShowDialog() == DialogResult.OK)
                 {
@@ -140,7 +129,7 @@ namespace AutomateCoA
             if(bcrMode == true)
             {
                 OpenFileDialog _fileDialog = new OpenFileDialog();
-                _fileDialog.InitialDirectory = "Z:\\SJShare\\SJCOMMON\\DI\\MIC\\COMPONENT DOCS & VALIDATIONS\\Quarantine Release\\Digital BCR - Test Set";
+                _fileDialog.InitialDirectory = "Z:\\SJShare\\SJCOMMON\\DI\\MIC\\COMPONENT DOCS & VALIDATIONS\\Quarantine Release\\Digital BCR - Lab Buddy";
 
                 if (_fileDialog.ShowDialog() == DialogResult.OK)
                 {
@@ -169,90 +158,92 @@ namespace AutomateCoA
         //find and save CoA
         private void FindCoA()
         {
-            //iniate web browser with console hidden.  Browser will close when driver is exited.
-            ChromeOptions options = new ChromeOptions();
-            options.LeaveBrowserRunning = false;
-
-            new DriverManager().SetUpDriver(new ChromeConfig());
-            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
-            service.HideCommandPromptWindow = true;
-            IWebDriver mydriver = new ChromeDriver(service, options);
-
-            //when waiting on elements to load
-            WebDriverWait wait = new WebDriverWait(mydriver, TimeSpan.FromSeconds(10));
-
-
-
-            //go find the stuff from vendors
+            
             try
             {
+                //iniate web browser with console hidden.  Browser will close when driver is exited.
+                ChromeOptions options = new ChromeOptions();
+                options.LeaveBrowserRunning = false;
+                ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+                new DriverManager().SetUpDriver(new ChromeConfig());
+
+
+                service.HideCommandPromptWindow = true;
+                IWebDriver myDriver = new ChromeDriver(service, options);
+
+                //when waiting on elements to load
+                WebDriverWait wait = new WebDriverWait(myDriver, TimeSpan.FromSeconds(10));
+
+                //get stuff from vendors
+
                 //get input from text/comboboxes
+
                 switch (VendorBox1.Text.ToUpper())
                 {
                     case "FISHER":
 
 
-                        mydriver.Url = "https://www.fishersci.com/us/en/catalog/search/certificates.html";
+                        myDriver.Url = "https://www.fishersci.com/us/en/catalog/search/certificates.html";
                         //catalog number
-                        mydriver.FindElement(By.XPath("/html/body/section/section/div/div/div/div[2]/div[1]/div/form/ul/li[1]/input")).SendKeys(ItemBox1.Text);
+                        myDriver.FindElement(By.XPath("/html/body/section/section/div/div/div/div[2]/div[1]/div/form/ul/li[1]/input")).SendKeys(ItemBox1.Text);
                         //lot number
-                        mydriver.FindElement(By.XPath("/html/body/section/section/div/div/div/div[2]/div[1]/div/form/ul/li[2]/input")).SendKeys(LotBox1.Text);
+                        myDriver.FindElement(By.XPath("/html/body/section/section/div/div/div/div[2]/div[1]/div/form/ul/li[2]/input")).SendKeys(LotBox1.Text);
                         //document type.  HAVE TO HIT RETURN TO CONFIRM SELECTION OR IT ALL GOES TO HELL!!!!
-                        mydriver.FindElement(By.XPath("/html/body/section/section/div/div/div/div[2]/div[1]/div/form/ul/li[3]/select")).SendKeys("C");
-                        mydriver.FindElement(By.XPath("/html/body/section/section/div/div/div/div[2]/div[1]/div/form/ul/li[3]/select")).SendKeys(OpenQA.Selenium.Keys.Return);
+                        myDriver.FindElement(By.XPath("/html/body/section/section/div/div/div/div[2]/div[1]/div/form/ul/li[3]/select")).SendKeys("C");
+                        myDriver.FindElement(By.XPath("/html/body/section/section/div/div/div/div[2]/div[1]/div/form/ul/li[3]/select")).SendKeys(OpenQA.Selenium.Keys.Return);
                         //search button
-                        mydriver.FindElement(By.CssSelector("#certificate-search-form > form > ul > li:nth-child(4) > input")).Click();
+                        myDriver.FindElement(By.CssSelector("#certificate-search-form > form > ul > li:nth-child(4) > input")).Click();
                         //certificate inside of table     
-                        mydriver.FindElement(By.XPath("/html/body/section/section/div/div/div/div[3]/table/tbody/tr/td[6]/a")).Click();
+                        myDriver.FindElement(By.XPath("/html/body/section/section/div/div/div/div[3]/table/tbody/tr/td[6]/a")).Click();
 
                         //close window but not exit webdriver
-                        mydriver.Close();
+                        myDriver.Close();
                         break;
 
                     case "SIGMA":
-                        mydriver.Url = "https://www.sigmaaldrich.com/US/en/search";
+                        myDriver.Url = "https://www.sigmaaldrich.com/US/en/search";
 
                         //click on cookie accept button
                         IWebElement cookiebtn = wait.Until(drvr => drvr.FindElement(By.CssSelector("#onetrust-close-btn-container > button")));
                         cookiebtn.Click();
 
                         //product number
-                        mydriver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div/div/div[2]/div/div[1]/form/div/div[1]/div/div/div/input")).SendKeys(ItemBox1.Text);
+                        myDriver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div/div/div[2]/div/div[1]/form/div/div[1]/div/div/div/input")).SendKeys(ItemBox1.Text);
                         //lot number and execture search
-                        mydriver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div/div/div[2]/div/div[1]/form/div/div[3]/div/div/div/input")).SendKeys(LotBox1.Text);
-                        mydriver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div/div/div[2]/div/div[1]/form/div/div[3]/div/div/div/input")).SendKeys(OpenQA.Selenium.Keys.Return);
+                        myDriver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div/div/div[2]/div/div[1]/form/div/div[3]/div/div/div/input")).SendKeys(LotBox1.Text);
+                        myDriver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div/div/div[2]/div/div[1]/form/div/div[3]/div/div/div/input")).SendKeys(OpenQA.Selenium.Keys.Return);
 
                         //TODO why cant I close the tab??!?! without closing all the things
-                        //mydriver.Close();
+                        //myDriver.Close();
 
                         break;
 
                     case "VWR":
-                        mydriver.Url = "https://us.vwr.com/store/search/searchCerts.jsp?tabId=certSearch";
+                        myDriver.Url = "https://us.vwr.com/store/search/searchCerts.jsp?tabId=certSearch";
 
                         //part number
 
-                        mydriver.FindElement(By.XPath("//*[@id='certSearchPartNumber']")).SendKeys(ItemBox1.Text);
+                        myDriver.FindElement(By.XPath("//*[@id='certSearchPartNumber']")).SendKeys(ItemBox1.Text);
                         //lot number
-                        mydriver.FindElement(By.XPath("//*[@id='certSearchLotNumber']")).SendKeys(LotBox1.Text);
+                        myDriver.FindElement(By.XPath("//*[@id='certSearchLotNumber']")).SendKeys(LotBox1.Text);
                         //search button
-                        mydriver.FindElement(By.XPath("//*[@id='certSearch']")).Click();
+                        myDriver.FindElement(By.XPath("//*[@id='certSearch']")).Click();
                         //certificate inside of table
-                        mydriver.FindElement(By.XPath("//*[@id='content']/div/div[2]/div[2]/div/div/table/tbody[1]/tr/td[2]/a")).Click();
+                        myDriver.FindElement(By.XPath("//*[@id='content']/div/div[2]/div[2]/div/div/table/tbody[1]/tr/td[2]/a")).Click();
 
-                        //mydriver.Close();
+                        //myDriver.Close();
 
                         break;
 
                     case "BD":
-                        mydriver.Url = "https://regdocs.bd.com/regdocs/qcinfo";
+                        myDriver.Url = "https://regdocs.bd.com/regdocs/qcinfo";
 
 
-                        mydriver.FindElement(By.XPath("//*[@id='page']/div/section[2]/div/div/div/div/div/form/div[2]/div[1]/div/div[1]/input")).SendKeys(ItemBox1.Text);
+                        myDriver.FindElement(By.XPath("//*[@id='page']/div/section[2]/div/div/div/div/div/form/div[2]/div[1]/div/div[1]/input")).SendKeys(ItemBox1.Text);
                         //lot number
-                        mydriver.FindElement(By.XPath("//*[@id='page']/div/section[2]/div/div/div/div/div/form/div[2]/div[1]/div/div[2]/input")).SendKeys(LotBox1.Text);
+                        myDriver.FindElement(By.XPath("//*[@id='page']/div/section[2]/div/div/div/div/div/form/div[2]/div[1]/div/div[2]/input")).SendKeys(LotBox1.Text);
                         //search button
-                        mydriver.FindElement(By.XPath("//*[@id='page']/div/section[2]/div/div/div/div/div/form/button[2]")).Click();
+                        myDriver.FindElement(By.XPath("//*[@id='page']/div/section[2]/div/div/div/div/div/form/button[2]")).Click();
                         //certificate inside of table
 
                         //click on cookie accept button
@@ -262,17 +253,17 @@ namespace AutomateCoA
                         break;
 
                     case "MILLIPORE":
-                        mydriver.Url = "https://www.emdmillipore.com/US/en/documents/Z.qb.qB.tecAAAFDDJUsznLq,nav#s_mbqBTWQAAAFKyVsIik_d";
+                        myDriver.Url = "https://www.emdmillipore.com/US/en/documents/Z.qb.qB.tecAAAFDDJUsznLq,nav#s_mbqBTWQAAAFKyVsIik_d";
                         //defeats automation blocking. BOOYAH!
-                        mydriver.Manage().Cookies.DeleteAllCookies();
+                        myDriver.Manage().Cookies.DeleteAllCookies();
 
                         //catalog number  
-                        mydriver.FindElement(By.XPath("//*[@id='COAOrderNumber']")).SendKeys(ItemBox1.Text);
+                        myDriver.FindElement(By.XPath("//*[@id='COAOrderNumber']")).SendKeys(ItemBox1.Text);
                         //lot number  
-                        mydriver.FindElement(By.XPath("//*[@id='COABatchNumber']")).SendKeys(LotBox1.Text);
-                        //mydriver.FindElement(By.XPath("//*[@id='COABatchNumber']")).SendKeys(OpenQA.Selenium.Keys.Return);
+                        myDriver.FindElement(By.XPath("//*[@id='COABatchNumber']")).SendKeys(LotBox1.Text);
+                        //myDriver.FindElement(By.XPath("//*[@id='COABatchNumber']")).SendKeys(OpenQA.Selenium.Keys.Return);
                         //search button
-                        mydriver.FindElement(By.XPath("//*[@id='s_mbqBTWQAAAFKyVsIik_d_find']")).SendKeys(OpenQA.Selenium.Keys.Return);
+                        myDriver.FindElement(By.XPath("//*[@id='s_mbqBTWQAAAFKyVsIik_d_find']")).SendKeys(OpenQA.Selenium.Keys.Return);
 
                         //certificate inside of table
                         //this will break  results is tied to individual coa.  leaving window open for user to manually click and download
@@ -281,15 +272,15 @@ namespace AutomateCoA
 
                         break;
                 }
-
             }
-            catch (Exception Exception)
+            catch (InvalidOperationException e)
             {
-                int errorLength = 100;
-
-                var myError = Exception;
-                mydriver.Quit();
-                MessageBox.Show("Tell Pete this happened: " + myError.ToString().Substring(0, errorLength));
+                MessageBox.Show(e.Message);
+                
+                if (e.Message.ToUpper().Contains("VERSION OF CHROME"))
+                {
+                    MessageBox.Show("Please Update To The Most Current Version of Chrome.");
+                }               
             }
         }
 
@@ -365,7 +356,7 @@ namespace AutomateCoA
                 finalPdf.AppendPage(secondPDF);
 
                 string finalPdfPathDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-                string finalPdfPathFiixFolder = "Z:\\SJShare\\SJCOMMON\\DI\\MIC\\COMPONENT DOCS & VALIDATIONS\\Fiix Scanned Documents";
+                string finalPdfPathFiixFolder = "Z:\\SJShare\\SJCOMMON\\DI\\MIC\\COMPONENT DOCS & VALIDATIONS\\Fiix Scanned Documents\\";
                 string finalPdfName = FinalPDFBx.Text;
 
                 if(finalPdfName != "")
@@ -642,19 +633,14 @@ namespace AutomateCoA
                     pb.Image = img;
                 }
             }
-
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             GeneratePTSCodes();
         }
-
         private void ClearPTS_Click(object sender, EventArgs e)
         {
             ClearControls();
         }
-
-     
     }
 }
